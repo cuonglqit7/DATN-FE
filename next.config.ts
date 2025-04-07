011a0efdf9d1d3d1079046b84a3e0a2d15ae061d
Le Quoc Cuong <139851615+cuonglqit7@users.noreply.github.com>
2025-04-08 02:22:34 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        domains: ['admin.teabliss.io.vn'],
-      },
-    ],
+    domains: ['admin.teabliss.io.vn'],
   },
   eslint: {
     ignoreDuringBuilds: true,
